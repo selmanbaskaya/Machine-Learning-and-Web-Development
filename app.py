fccdd7e7d78edba67aa774631776c35ac262aacc
Selman <selmanbaskaya1@gmail.com>
2021-04-17 17:12:04 +0300

    sorce codes added for fifth video

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/predict', methods=['POST'])
+def predict():
+	print("entered!")
+	return render_template('index.html')
 
 if __name__ == 'main':
 	app.run(debug=True)
