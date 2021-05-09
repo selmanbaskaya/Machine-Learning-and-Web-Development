c94a2f99f75e2ab990fdb185d41b0805c7006c86
Selman <selmanbaskaya1@gmail.com>
2021-05-09 15:30:34 +0300

    sorce codes added for sixth video

--- app.py
+++ app.py
@@ -1,6 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
+from flask import request
+import numpy as np
+import pickle
 
 app = Flask(__name__)
+
+def setMethodValues(value):
+	if value == 'sync':
+		return 1, 0, 0
+	elif value == 'async':
+		return 0, 1, 0
+	else:
+		return 0, 0, 1
+
+def setMechanimsValues(value):
+	if value == 'theoric':
+		return 1
+	else:
+		return 0
+
+def setExamInteractionContact(value):
+	if value == 'yes':
+		return 1
+	else:
+		return 0
 
 @app.route('/')
 def index():
@@ -8,8 +32,17 @@ def index():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-	print("entered!")
-	return render_template('index.html')
+	methodSync, methodAsync, methodBoth = setMethodValues(request.form.get('method'))
+	mechanism = setMechanimsValues(request.form.get('mechanism'))
+	exam = setExamInteractionContact(request.form.get('exam'))
+	interaction = setExamInteractionContact(request.form.get('interaction'))
+	contact = setExamInteractionContact(request.form.get('contact'))
+
+	resultValues = np.array([[methodSync, methodAsync, methodBoth, mechanism, exam, interaction, contact]])
+	model = pickle.load(open('model.pkl', 'rb'))
+	predictionValue = model.predict(resultValues)
+
+	return render_template('index.html', prediction_text=predictionValue)
 
 if __name__ == 'main':
 	app.run(debug=True)
